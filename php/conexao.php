af2a291b0a5d6342965c0427e1836e9587f95dd5
DevYayaFarias <minvicflg@gmail.com>
2026-05-17 19:21:52 -0300

    Refactor database connection in conexao.php
    Updated database connection handling to include port and improved error logging.

--- php/conexao.php
+++ php/conexao.php
@@ -1,11 +1,17 @@
 <?php
 // Variáveis de conexão com o Banco de Dados
-$servidor = "localhost:3306";
+$servidor = "localhost";
+$porta = 3306;
 $usuario  = "root";
-$senha    = "PUC@1234";
+$senha    = "";
 $nome_banco = "lumind_db";
 
-$conexao = new mysqli($servidor, $usuario, $senha, $nome_banco);
-if($conexao->connect_error){
-    echo $conexao->connect_error;
+$conexao = new mysqli($servidor, $usuario, $senha, $nome_banco, $porta);
+if ($conexao->connect_error) {
+    error_log('MySQL Connect Error: ' . $conexao->connect_error);
+    // Em APIs retornamos JSON quando apropriado; aqui apenas interrompemos o script
+    die('Erro ao conectar ao banco de dados. Verifique os logs do servidor.');
 }
+
+// Compatibilidade: alguns scripts usam $mysqli em vez de $conexao
+$mysqli = $conexao;
